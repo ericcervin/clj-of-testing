afeeb912bd2eaab9bbc55d70cb2b82395f87d181
Eric Ervin <ericcervin@gmail.com>
2018-09-01 14:57:19 -0400

    added all-sites-have-robots test

--- src/clj_of_testing/core.clj
+++ src/clj_of_testing/core.clj
@@ -1,11 +1,21 @@
 (ns clj-of-testing.core
   (:require [clj-http.client :as client]
-            [clojure.test :refer [is]]))
+            [clojure.test :refer [is deftest run-tests]]))
+
+(def all-sites ["http://ericervin.org" 
+                "http://ericervin.com"
+                "http://ericcervin.github.io"
+                "http://noiselife.org"])
+
+(deftest sites-up 
+  (is (= [200 200 200 200] (mapv #(:status (client/get %)) all-sites))))
+
+(deftest all-sites-have-robots
+  (let [robot-urls (mapv #(str % "/robots.txt") all-sites)
+        robot-bodies (mapv #(:body (client/get %)) robot-urls)]
+       (is (every? #(clojure.string/includes? % "User-agent: *\nDisallow:") robot-bodies))))
 
 (defn -main []
-  (let [all-sites ["http://ericervin.org" 
-                   "http://ericervin.com"
-                   "http://ericcervin.github.io"
-                   "http://noiselife.org"]]
+  (run-tests))
             
-   (is (= [200 200 200 300] (mapv #(:status (client/get %)) all-sites)))))
+   
