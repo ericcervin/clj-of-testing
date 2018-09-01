60127f0113b3a26ed9eb02c8f7924fec002fe60e
Eric Ervin <ericcervin@gmail.com>
2018-09-01 16:17:37 -0400

    added all-sites-have-404s test

--- src/clj_of_testing/core.clj
+++ src/clj_of_testing/core.clj
@@ -8,12 +8,23 @@
                 "http://noiselife.org"])
 
 (deftest sites-up 
-  (is (= [200 200 200 200] (mapv #(:status (client/get %)) all-sites))))
+  (is (every? #(= 200 %) (mapv #(:status (client/get %)) all-sites))))
 
 (deftest all-sites-have-robots
   (let [robot-urls (mapv #(str % "/robots.txt") all-sites)
         robot-bodies (mapv #(:body (client/get %)) robot-urls)]
        (is (every? #(clojure.string/includes? % "User-agent: *\nDisallow:") robot-bodies))))
+
+(deftest all-sites-have-404s
+  (let [all-sites (filter #(not= "http://ericcervin.github.io" %) all-sites)
+        failing-urls (mapv #(str % "/platypus") all-sites)
+        failing-responses (mapv #(client/get % {:throw-exceptions false}) failing-urls)
+        failing-statuses (mapv :status failing-responses)
+        failing-bodies (mapv :body failing-responses)] 
+    (is (every? #(= 404 %) failing-statuses))
+    (is (every? #(clojure.string/includes? % "<title>Error 404 Not Found</title>") failing-bodies))
+    (is (every? #(clojure.string/includes? % "<body>404 - Not Found</body>") failing-bodies))))
+
 
 (defn -main []
   (run-tests))
